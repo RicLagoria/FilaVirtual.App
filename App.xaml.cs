e7ad5ded4eec5f804b382335275974dc4446757b
RicLagoria <ricardolagoria@gmail.com>
2025-10-20 15:05:10 -0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FilaVirtual.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
